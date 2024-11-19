947abf03e17a0425831bc31ea16abe8a052a6557
Yash Hegde <yash4success@gmail.com>
2024-11-19 18:30:19 +0530

    navbar border

--- src/components/ThumbnailPageComponents/ThumbnailNavbar.tsx
+++ src/components/ThumbnailPageComponents/ThumbnailNavbar.tsx
@@ -1,6 +1,6 @@
 function ThumbnailNavbar() {
   return (
-    <div className="flex justify-between items-center mx-10 py-4">
+    <div className="flex justify-between items-center px-10 py-4 sticky top-0 bg-white border border-b">
       <div>Thumbnail</div>
       <div className="flex items-center space-x-3">
         <div>About</div>
