86e0e82fea65f88975e9b63a8c79ed8ca474fb4d
Yash Hegde <yash4success@gmail.com>
2024-11-19 17:22:33 +0530

    hero section 1 done

--- src/pages/Thumbnail/Thumbail.tsx
+++ src/pages/Thumbnail/Thumbail.tsx
@@ -1,9 +1,11 @@
+import HeroSection1 from "@/components/ThumbnailPageComponents/HeroSection1";
 import ThumbnailNavbar from "@/components/ThumbnailPageComponents/ThumbnailNavbar";
 
 function ThumbailPage() {
   return (
     <div>
       <ThumbnailNavbar />
+      <HeroSection1 />
     </div>
   );
 }
