cef277e1e496dc2ecb8cf9ba05fc244193f4f4d8
NikhilK <nkumar.nk151221@gmail.com>
2025-01-17 11:12:52 +0530

    incomit

--- src/components/DescriptionPageComponents/HeroSection1.tsx
+++ src/components/DescriptionPageComponents/HeroSection1.tsx
@@ -2,30 +2,23 @@ import { Button } from "../ui/button";
 
 function HeroSection1() {
   return (
-
     <>
-    <div className="flex flex-col justify-center items-center h-auto md:h-80 px-4">
-      <div className="text-wrap w-full md:w-1/2 text-center space-y-5">
-        <div className="text-3xl md:text-5xl font-semibold leading-tight">
-          Generate Stunning thumbnails for your videos
-        </div>
-        <div className="font-thin text-sm md:text-lg space-y-2">
-          <div>
-            Elevate your content with our powerful AI-driven thumbnail generator
+      <div className="flex flex-col justify-center items-center h-auto md:h-80 px-4">
+        <div className="text-wrap w-full md:w-1/2 text-center space-y-5">
+          <div className="text-3xl md:text-5xl font-semibold leading-tight">
+            Create Engaging and Optimized Descriptions for Your YouTube Videos
           </div>
-          <div>Effortlessly create eye-catching content</div>
+          <div className="font-thin text-sm md:text-lg space-y-2">
+            <div>Elevate your content with our powerful AI-driven tools</div>
+            <div>Effortlessly Craft Captivating Video Descriptions</div>
+          </div>
         </div>
+        <Button className="mt-6 md:mt-8" variant="destructive">
+          Try it Now
+        </Button>
       </div>
-      <Button className="mt-6 md:mt-8" variant="destructive">
-        Try it Now
-      </Button>
-      
-
-    </div>
-<div style={{height:'3%',visibility:'hidden'}}>h</div>
-   
+      <div style={{ height: "3%", visibility: "hidden" }}>h</div>
     </>
-
   );
 }
 
--- src/components/ScriptPageComponents/HeroSection1.tsx
+++ src/components/ScriptPageComponents/HeroSection1.tsx
@@ -2,30 +2,23 @@ import { Button } from "../ui/button";
 
 function HeroSection1() {
   return (
-
     <>
-    <div className="flex flex-col justify-center items-center h-auto md:h-80 px-4">
-      <div className="text-wrap w-full md:w-1/2 text-center space-y-5">
-        <div className="text-3xl md:text-5xl font-semibold leading-tight">
-          Generate Stunning thumbnails for your videos
-        </div>
-        <div className="font-thin text-sm md:text-lg space-y-2">
-          <div>
-            Elevate your content with our powerful AI-driven thumbnail generator
+      <div className="flex flex-col justify-center items-center h-auto md:h-80 px-4">
+        <div className="text-wrap w-full md:w-1/2 text-center space-y-5">
+          <div className="text-3xl md:text-5xl font-semibold leading-tight">
+            Generate Compelling Scripts for Your YouTube Videos
           </div>
-          <div>Effortlessly create eye-catching content</div>
+          <div className="font-thin text-sm md:text-lg space-y-2">
+            <div>Elevate your content with our powerful AI-driven tools</div>
+            <div>Effortlessly Craft Engaging Video Scripts</div>
+          </div>
         </div>
+        <Button className="mt-6 md:mt-8" variant="destructive">
+          Try it Now
+        </Button>
       </div>
-      <Button className="mt-6 md:mt-8" variant="destructive">
-        Try it Now
-      </Button>
-      
-
-    </div>
-<div style={{height:'3%',visibility:'hidden'}}>h</div>
-   
+      <div style={{ height: "3%", visibility: "hidden" }}>h</div>
     </>
-
   );
 }
 
