ee5fe4929a4ff78e043ebae3e771600d7f4b23bf
Yash Hegde <yash4success@gmail.com>
2025-04-20 11:15:13 +0530

    logo on navbar

--- src/components/Chat/ChatNavbar.tsx
+++ src/components/Chat/ChatNavbar.tsx
@@ -2,8 +2,15 @@
 const ChatNavbar = () => {
  
   return (
-    <div className="bg-chatOverlay font-sans my-2 mx-6 rounded-md">
-      chatNavbar
+    <div className="bg-chatOverlay font-sans my-2 mx-6 rounded-md flex justify-between py-2 px-4">
+      <div className="flex space-x-2 justify-center items-center">
+        <img src="/logo.png" alt="logo" className="h-10 w-10 border rounded-md border border-sky-500" />
+        <div className="text-2xl">
+          Lithouse
+        </div>
+      </div>
+      <div>hi</div>
+
     </div>
   );
 };
