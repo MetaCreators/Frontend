2307f8cd7c6fd3e9ead38809fa37f70fce70dcd8
Yash Hegde <yash4success@gmail.com>
2025-05-13 08:09:17 +0530

    card stacking WIP

--- src/components/LandingPage/FeatureCard.tsx
+++ src/components/LandingPage/FeatureCard.tsx
@@ -13,7 +13,11 @@ export default function FeatureCard({ id, color, description, title }: {
     })
     const scale = useTransform(scrollYProgress,[0,1],[2,1])
     return (
-        <div ref={container} className="h-screen flex items-center justify-center sticky -top-3">
+        <div 
+            ref={container} 
+            className="h-screen flex items-center justify-center sticky"
+            style={{ top: `calc(10% + ${(id) * 2}%)` }}
+        >
             <div className={`p-6 rounded-lg ${color} text-black w-[1000px] h-[80vh]`}>
                 <h3 className="text-xl font-bold mb-2">{title}</h3>
                 <p>{description}</p>
